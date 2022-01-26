2d0261643f5d742f1ea64a8bb7276fd475d4819a
g98garima <98175990+g98garima@users.noreply.github.com>
2022-01-26 23:49:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function Calculate()
+    {
+        let p=document.getElementById('amount').value;
+        let t=document.getElementById('time').value;
+        let r=document.getElementById('textInput').value;
+
+        let  SI=(p*t*r)/100;
+
+        document.getElementById('si').innerHTML="If you deposit "+p+"<br>at an interest rate of "+r+"<br>You will receive an amount of " +SI;
+    }
+    function updateTextInput(val) {
+        document.getElementById('textInput').value=val; 
+      }
