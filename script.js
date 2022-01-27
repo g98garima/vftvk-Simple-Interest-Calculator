1b2f35cdfb64bf0507cb60b82749342713374a85
g98garima <98175990+g98garima@users.noreply.github.com>
2022-01-28 00:02:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ function Calculate()
         let t=document.getElementById('time').value;
         let r=document.getElementById('textInput').value;
 
-        let  SI=(p*t*r)/100;
-        if (p==0) 
+        
+        if (p==0) {
             alert("enter a positive number!")
-        else if (p<0)
+            document.getElementById('si').innerHTML=''
+        }
+        else if (p<0){
             alert("enter a positive number")
+            document.getElementById('si').innerHTML=''
+        }
+        else{
+            let  SI=(p*t*r)/100;
        
-        document.getElementById('si').innerHTML="If you deposit <mark>"+p+"</mark>"+"<br>at an interest rate of <mark> "+r+"</mark>"+"<br>You will receive an amount of <mark>" +SI+"</mark>";
- 
+            document.getElementById('si').innerHTML="If you deposit <mark>"+p+"</mark>"+"<br>at an interest rate of <mark> "+r+"</mark>"+"<br>You will receive an amount of <mark>" +SI+"</mark>";
+        }
     }
     function updateTextInput(val) {
         document.getElementById('textInput').value=val; 
